db6c5afbdef36a69855be76b881d86b9a771a9ca
Jaime BERNABE <jbernabe@e2r10p12.42.fr>
2015-06-11 12:36:10 +0200

    base with inherits

--- wiboBase/Wibo_db_create.sql
+++ wiboBase/Wibo_db_create.sql
@@ -1,321 +1,250 @@
 -- Created by Vertabelo (http://vertabelo.com)
--- Last modification date: 2015-06-10 07:23:13.02
+-- Last modification date: 2015-06-11 08:29:39.327
 
 
 
 
 -- tables
--- Table: Checkpoints
-CREATE TABLE Checkpoints (
-    id int  NOT NULL,
+
+-- Table: type_message
+CREATE TABLE type_message (
+    id_type_m int  NOT NULL,
+    typeName varchar(255)  NOT NULL,
+    CONSTRAINT type_message_pk PRIMARY KEY (id_type_m)
+);
+
+-- Table: type_container
+CREATE TABLE type_container (
+    id_type_c int  NOT NULL,
+    typeName varchar(255)  NOT NULL,
+    CONSTRAINT type_container_pk PRIMARY KEY (id_type_c)
+);
+
+
+
+-- Table: type_device
+CREATE TABLE type_device (
+    id_type_d int  NOT NULL,
+    typeName varchar(255)  NOT NULL,
+    CONSTRAINT type_device_pk PRIMARY KEY (id_type_d)
+);
+
+
+
+-- Table: type_group
+CREATE TABLE type_group (
+    id_type_g int  NOT NULL,
+    groupName varchar(20)  NOT NULL,
+    CONSTRAINT type_group_pk PRIMARY KEY (id_type_g)
+);
+
+
+-- Table: type_information
+CREATE TABLE type_information (
+    id_type_info int  NOT NULL,
+    name_info varchar(255)  NOT NULL,
+    CONSTRAINT type_information_pk PRIMARY KEY (id_type_info)
+);
+-- Table: checkpoints
+CREATE TABLE checkpoints (
+    id serial  NOT NULL,
     longitude decimal(5,2)  NOT NULL,
     latitude decimal(5,2)  NOT NULL,
     date date  NOT NULL,
     containerId int  NOT NULL,
     attractByMagnet boolean  NOT NULL,
-    CONSTRAINT Checkpoints_pk PRIMARY KEY (id)
+    CONSTRAINT checkpoints_pk PRIMARY KEY (id)
 );
 
-CREATE INDEX Checkpoints_container_id_idx on Checkpoints (containerId ASC,date ASC);
+CREATE INDEX Checkpoints_container_id_idx on checkpoints (containerId ASC,date ASC);
 
 
 
 
--- Table: Container
-CREATE TABLE Container (
-    id int  NOT NULL,
+-- Table: container
+CREATE TABLE container (
+    id serial  NOT NULL,
     longitude decimal(5,2)  NOT NULL,
     latitude decimal(5,2)  NOT NULL,
     direction decimal(5,2)  NOT NULL,
     speed int  NOT NULL,
     creationDate date  NOT NULL,
-    userId int  NOT NULL,
-    typeContainerId int  NOT NULL,
-    titre varchar(255) NOT NULL, 
-    CONSTRAINT Container_pk PRIMARY KEY (id)
-);
+    Device_id int  NOT NULL,
+    CONSTRAINT container_pk PRIMARY KEY (id)
+) INHERITS (type_container);
 
 
 
--- Table: Device
-CREATE TABLE Device (
-    id int  NOT NULL,
+-- Table: device
+CREATE TABLE device (
+    id serial  NOT NULL,
     macAddr varchar(18)  NOT NULL,
-    typeDeviceId int  NOT NULL,
-    idUser int  NOT NULL,
-    user_id int  NOT NULL,
-    CONSTRAINT Device_pk PRIMARY KEY (id)
-);
+    user_id_user int  NOT NULL,
+    lastUseMagnet date  NOT NULL,
+    CONSTRAINT device_pk PRIMARY KEY (id)
+) INHERITS (type_device);
 
 
 
--- Table: Followed
-CREATE TABLE Followed (
-    id int  NOT NULL,
-    User_id int  NOT NULL,
+-- Table: followed
+CREATE TABLE followed (
+    id serial  NOT NULL,
     Container_id int  NOT NULL,
-    CONSTRAINT Followed_pk PRIMARY KEY (id)
+    Device_id int  NOT NULL,
+    CONSTRAINT followed_pk PRIMARY KEY (id)
 );
 
 
 
--- Table: "Group"
-CREATE TABLE "Group" (
-    id int  NOT NULL,
-    groupName varchar(20)  NOT NULL,
-    CONSTRAINT Group_pk PRIMARY KEY (id)
-);
-
-
-
--- Table: Message
-CREATE TABLE Message (
-    id int  NOT NULL,
+-- Table: message
+CREATE TABLE message (
+    id serial  NOT NULL,
     content text  NOT NULL,
     containerId int  NOT NULL,
-    userId int  NOT NULL,
-    typeMessageId int  NOT NULL,
-    CONSTRAINT Message_pk PRIMARY KEY (id)
-);
+    Device_id int  NOT NULL,
+    CONSTRAINT message_pk PRIMARY KEY (id)
+) INHERITS (type_message);
 
-CREATE INDEX Message_idx_container on Message (containerId ASC);
-
+CREATE INDEX Message_idx_container on message (containerId ASC);
 
 
 
--- Table: Reception
-CREATE TABLE Reception (
-    id int  NOT NULL,
+
+-- Table: reception
+CREATE TABLE reception (
+    id serial  NOT NULL,
     receptionTime date  NOT NULL,
     longitude decimal(5,2)  NOT NULL,
     latitude decimal(5,2)  NOT NULL,
-    userId int  NOT NULL,
     IdContainer int  NOT NULL,
-    CONSTRAINT Reception_pk PRIMARY KEY (id)
-);
-
-
-
--- Table: Session
-CREATE TABLE Session (
-    id int  NOT NULL,
-    logged boolean  NOT NULL,
-    loginTime date  NOT NULL,
-    logoutTime boolean  NOT NULL,
-    userId int  NULL,
-    CONSTRAINT Session_pk PRIMARY KEY (id)
-);
-
-
-
--- Table: Type_Container
-CREATE TABLE Type_Container (
-    id int  NOT NULL,
-    typeName varchar(255)  NOT NULL,
-    CONSTRAINT Type_Container_pk PRIMARY KEY (id)
-);
-
-
-
--- Table: Type_Device
-CREATE TABLE Type_Device (
-    id int  NOT NULL,
-    typeName varchar(255)  NOT NULL,
-    CONSTRAINT Type_Device_pk PRIMARY KEY (id)
-);
-
-
-
--- Table: Type_Message
-CREATE TABLE Type_Message (
-    id int  NOT NULL,
-    typeName varchar(255)  NOT NULL,
-    CONSTRAINT Type_Message_pk PRIMARY KEY (id)
+    Device_id int  NOT NULL,
+    CONSTRAINT reception_pk PRIMARY KEY (id)
 );
 
 
 
 -- Table: shared
 CREATE TABLE shared (
-    id int  NOT NULL,
+    id serial  NOT NULL,
     type_shared int  NOT NULL,
-    User_id int  NOT NULL,
+    Device_id int  NOT NULL,
     CONSTRAINT shared_pk PRIMARY KEY (id)
-);
+) INHERITS (type_information);
+
+
+
+
+
 
 
 
 -- Table: "user"
 CREATE TABLE "user" (
-    id int  NOT NULL,
+    id_user serial  NOT NULL,
     login varchar(255)  NOT NULL,
     password varchar(255)  NOT NULL,
     salt varchar(255)  NOT NULL,
     lastLogin date  NOT NULL,
     creationDate date  NOT NULL,
     mail varchar(510)  NOT NULL,
-    groupId int  NOT NULL,
-    UseMagnet date  NOT NULL,
-    CONSTRAINT user_pk PRIMARY KEY (id)
-);
+    CONSTRAINT user_pk PRIMARY KEY (id_user)
+) INHERITS (type_group);
 
--- Table: "container_type_information"
-CREATE TABLE container_type_information (
-    id_type integer NOT NULL,
-    name_type character varying(255) NOT NULL,
-    CONSTRAINT container_type_information_pk PRIMARY KEY (id_type)
-);
+
+
+
 
 
 
 -- foreign keys
--- Reference:  Checkpoints_Container (table: Checkpoints)
+-- Reference:  Checkpoints_Container (table: checkpoints)
 
 
-ALTER TABLE Checkpoints ADD CONSTRAINT Checkpoints_Container 
+ALTER TABLE checkpoints ADD CONSTRAINT Checkpoints_Container
     FOREIGN KEY (containerId)
-    REFERENCES Container (id)
-    NOT DEFERRABLE 
-    INITIALLY IMMEDIATE 
-;
-
--- Reference:  Container_Type_Container (table: Container)
-
-
-ALTER TABLE Container ADD CONSTRAINT Container_Type_Container 
-    FOREIGN KEY (typeContainerId)
-    REFERENCES Type_Container (id)
-    NOT DEFERRABLE 
-    INITIALLY IMMEDIATE 
-;
-
--- Reference:  Container_User (table: Container)
-
-
-ALTER TABLE Container ADD CONSTRAINT Container_User 
-    FOREIGN KEY (userId)
-    REFERENCES "user" (id)
-    NOT DEFERRABLE 
-    INITIALLY IMMEDIATE 
-;
-
--- Reference:  Device_Type_Device (table: Device)
-
-
-ALTER TABLE Device ADD CONSTRAINT Device_Type_Device 
-    FOREIGN KEY (typeDeviceId)
-    REFERENCES Type_Device (id)
-    NOT DEFERRABLE 
-    INITIALLY IMMEDIATE 
-;
-
--- Reference:  Device_user (table: Device)
-
-
-ALTER TABLE Device ADD CONSTRAINT Device_user 
-    FOREIGN KEY (user_id)
-    REFERENCES "user" (id)
-    NOT DEFERRABLE 
-    INITIALLY IMMEDIATE 
-;
-
--- Reference:  Followed_Container (table: Followed)
-
-
-ALTER TABLE Followed ADD CONSTRAINT Followed_Container 
-    FOREIGN KEY (Container_id)
-    REFERENCES Container (id)
-    NOT DEFERRABLE 
-    INITIALLY IMMEDIATE 
-;
-
--- Reference:  Followed_User (table: Followed)
-
-
-ALTER TABLE Followed ADD CONSTRAINT Followed_User 
-    FOREIGN KEY (User_id)
-    REFERENCES "user" (id)
-    NOT DEFERRABLE 
-    INITIALLY IMMEDIATE 
-;
-
--- Reference:  Message_Container (table: Message)
-
-
-ALTER TABLE Message ADD CONSTRAINT Message_Container 
-    FOREIGN KEY (containerId)
-    REFERENCES Container (id)
-    NOT DEFERRABLE 
-    INITIALLY IMMEDIATE 
-;
-
--- Reference:  Message_Type_message (table: Message)
-
-
-ALTER TABLE Message ADD CONSTRAINT Message_Type_message 
-    FOREIGN KEY (typeMessageId)
-    REFERENCES Type_Message (id)
-    NOT DEFERRABLE 
-    INITIALLY IMMEDIATE 
-;
-
--- Reference:  Message_User (table: Message)
-
-
-ALTER TABLE Message ADD CONSTRAINT Message_User 
-    FOREIGN KEY (userId)
-    REFERENCES "user" (id)
-    NOT DEFERRABLE 
-    INITIALLY IMMEDIATE 
-;
-
--- Reference:  Reception_User (table: Reception)
-
-
-ALTER TABLE Reception ADD CONSTRAINT Reception_User 
-    FOREIGN KEY (userId)
-    REFERENCES "user" (id)
-    NOT DEFERRABLE 
-    INITIALLY IMMEDIATE 
-;
-
--- Reference:  "Session_User" (table: Session)
-
-
-ALTER TABLE Session ADD CONSTRAINT "Session_User" 
-    FOREIGN KEY (userId)
-    REFERENCES "user" (id)
-    NOT DEFERRABLE 
-    INITIALLY IMMEDIATE 
-;
-
--- Reference:  Shared_User (table: shared)
-
-
-ALTER TABLE shared ADD CONSTRAINT Shared_User 
-    FOREIGN KEY (User_id)
-    REFERENCES "user" (id)
-    NOT DEFERRABLE 
-    INITIALLY IMMEDIATE 
-;
-
--- Reference: Shared_container_type_information 
-
-ALTER TABLE container_type_information ADD CONSTRAINT Shared_container_type_information 
-    FOREIGN KEY (type_shared)
-    REFERENCES container_type_information (id_type)
-    NOT DEFERRABLE 
+    REFERENCES container (id)
+    NOT DEFERRABLE
     INITIALLY IMMEDIATE
 ;
 
--- Reference:  User_Group (table: "user")
+-- Reference:  Container_Device (table: container)
 
 
-ALTER TABLE "user" ADD CONSTRAINT User_Group 
-    FOREIGN KEY (groupId)
-    REFERENCES "Group" (id)
-    NOT DEFERRABLE 
-    INITIALLY IMMEDIATE 
+ALTER TABLE container ADD CONSTRAINT Container_Device
+    FOREIGN KEY (Device_id)
+    REFERENCES device (id)
+    NOT DEFERRABLE
+    INITIALLY IMMEDIATE
+;
+
+-- Reference:  Followed_Container (table: followed)
+
+
+ALTER TABLE followed ADD CONSTRAINT Followed_Container
+    FOREIGN KEY (Container_id)
+    REFERENCES container (id)
+    NOT DEFERRABLE
+    INITIALLY IMMEDIATE
+;
+
+-- Reference:  Followed_Device (table: followed)
+
+
+ALTER TABLE followed ADD CONSTRAINT Followed_Device
+    FOREIGN KEY (Device_id)
+    REFERENCES device (id)
+    NOT DEFERRABLE
+    INITIALLY IMMEDIATE
+;
+
+-- Reference:  Message_Container (table: message)
+
+
+ALTER TABLE message ADD CONSTRAINT Message_Container
+    FOREIGN KEY (containerId)
+    REFERENCES container (id)
+    NOT DEFERRABLE
+    INITIALLY IMMEDIATE
+;
+
+-- Reference:  Message_Device (table: message)
+
+
+ALTER TABLE message ADD CONSTRAINT Message_Device
+    FOREIGN KEY (Device_id)
+    REFERENCES device (id)
+    NOT DEFERRABLE
+    INITIALLY IMMEDIATE
+;
+
+-- Reference:  Reception_Device (table: reception)
+
+
+ALTER TABLE reception ADD CONSTRAINT Reception_Device
+    FOREIGN KEY (Device_id)
+    REFERENCES device (id)
+    NOT DEFERRABLE
+    INITIALLY IMMEDIATE
+;
+
+-- Reference:  device_user (table: device)
+
+
+ALTER TABLE device ADD CONSTRAINT device_user
+    FOREIGN KEY (user_id_user)
+    REFERENCES "user" (id_user)
+    NOT DEFERRABLE
+    INITIALLY IMMEDIATE
+;
+
+-- Reference:  shared_Device (table: shared)
+
+
+ALTER TABLE shared ADD CONSTRAINT shared_Device
+    FOREIGN KEY (Device_id)
+    REFERENCES device (id)
+    NOT DEFERRABLE
+    INITIALLY IMMEDIATE
 ;
 
 
@@ -324,4 +253,3 @@ ALTER TABLE "user" ADD CONSTRAINT User_Group
 
 
 -- End of file.
-
